1a55d84e6b0cb5472d183f4bb557e6b8459f3798
elst <elstxds@proton.me>
2024-05-28 10:22:17 +0500

    pycharm's cache deleted

--- utils/users.py
+++ utils/users.py
@@ -1,16 +1,17 @@
 from utils import hash_password
-def add_user(connection, username, name, surname, password, role, pic=""):
-    connection.open()
-    cursor = connection.cursor()
-    password = hash_password(password)
-    querry = (f"INSERT INTO users (username, name, surname, password, role, picture) VALUES (%s, %s, %s, %s, %s, %s);")
-    try:
-        cursor.execute(querry, (username, name, surname, password, role, pic))
-    except Exception as ex:
-        print("[Info] Ошибка добавления данных:", ex)
-    finally:
-        cursor.close()
-        connection.close()
+import sqlite3
+def add_user(username, name, surname, password, role, pic=""):
+    with sqlite3.connect("data/manul.db") as con:
+        cur = con.cursor()
+        password = hash_password(password)
+        querry = (f"INSERT INTO users (username, name, surname, password, role, picture) VALUES (%s, %s, %s, %s, %s, %s);")
+        try:
+            cursor.execute(querry, (username, name, surname, password, role, pic))
+        except Exception as ex:
+            print("[Info] Ошибка добавления данных:", ex)
+        finally:
+            cursor.close()
+            connection.close()
 
 
 def rm_user(connection, user_id):
